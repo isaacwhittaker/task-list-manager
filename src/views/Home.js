14f50111055c4c0c7ba0ce7af35d47f00a2067e7
Isaac Whittaker <isaacw444@gmail.com>
2018-10-13 16:54:28 -0600

    Added rudimentary routing

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        Hello.
+        <NavLink to={`/tasks/`}>
+          <button>Tasks</button>
+        </NavLink>
       </div>
     );
   }
