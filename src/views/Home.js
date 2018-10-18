2f6ef8847abbc411ff91908071cecebe70e7d2cb
Isaac Whittaker <isaacw444@gmail.com>
2018-10-17 22:03:30 -0600

    Fixed authorization issue

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Auth } from 'aws-amplify';
 
 class Home extends Component {
+
+  state = { isManager: false }
+
+  async componentWillMount() {
+    let user = await Auth.currentAuthenticatedUser();
+    let accessGroups = user.signInUserSession.accessToken.payload['cognito:groups'];
+    if (accessGroups.indexOf("ManagerGroup") > -1) {
+      this.setState({ isManager: true });
+    }
+  }
+
   render() {
     return (
       <div className="container">
@@ -15,16 +27,23 @@ class Home extends Component {
           </div>
         </div>
         <br/>
-        <div className="row">
-          <h4>Manager Actions:</h4>
-        </div>
-        <div className="row">
-          <div className="btn-group btn-group-justified col-sm-6">
-            <NavLink to="/all-tasks" type="button" className="btn btn-primary">All Tasks</NavLink>
-            <NavLink to="/templates" type="button" className="btn btn-primary">Templates</NavLink>
-            <NavLink to="/create-template" type="button" className="btn btn-primary">Create Template</NavLink>
-          </div>
-        </div>
+
+        { this.state.isManager ?
+            <div>
+              <div className="row">
+                <h4>Manager Actions:</h4>
+              </div>
+              <div className="row">
+                <div className="btn-group btn-group-justified col-sm-6">
+                  <NavLink to="/all-tasks" type="button" className="btn btn-primary">All Tasks</NavLink>
+                  <NavLink to="/templates" type="button" className="btn btn-primary">Templates</NavLink>
+                  <NavLink to="/create-template" type="button" className="btn btn-primary">Create Template</NavLink>
+                </div>
+              </div>
+            </div>
+          : null
+        }
+
       </div>
     );
   }
